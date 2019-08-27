70344613e1f1a6e12ec605d74024cb29ba86337e
Kevin Ahumada <kevinahumada7@gmail.com>
2019-08-27 11:33:09 -0300

    Clase 03

---
+++ Ahumada.Kevin/Clase 03/Persona.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clase_03
+{
+    class Persona
+    {
+        public string nombre;
+        public string apellido;
+        public int dni;
+        
+
+        public Persona()
+        {
+            this.nombre = "Default";
+            this.apellido = "Default";
+        }
+
+        public string Mostrar()
+        {
+            return this.nombre + " " + this.apellido + " - " + this.dni.ToString();
+        }
+    }
+}
---
+++ Ahumada.Kevin/Clase 03/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Clase_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona usuario1 = new Persona();
+            Persona usuario2 = new Persona();
+            Persona usuario3 = new Persona();
+
+            usuario1.nombre = "Kevin";
+            usuario1.apellido = "Ahumada";
+            usuario1.dni = 43021356;
+
+            usuario2.nombre = "Luchettinni";
+            usuario2.apellido = "Aranda";
+            usuario2.dni = 42020715;
+
+
+            Console.WriteLine(usuario1.Mostrar());
+            Console.WriteLine(usuario2.Mostrar());
+            Console.WriteLine(usuario3.Mostrar());
+
+            Console.ReadLine();
+
+        }
+    }
+}
