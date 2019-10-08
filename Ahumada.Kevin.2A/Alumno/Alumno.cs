67689a7fd6909b1b85bd660ad52a38673d6df166
alumno <alumno@utnfra.com.ar>
2019-10-08 11:29:45 -0300

    Practica para Primer Parcial

---
+++ Ahumada.Kevin.2A/Alumno/Alumno.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Alumno
+    {
+        string _apellido;
+        int _legajo;
+        string _nombre;
+        float _nota;
+
+        public string Apellido { get { return this._apellido; } set { this._apellido = value; } }
+
+        public int Legajo { get { return this._legajo; } set { this._legajo = value; } }
+
+        public string Nombre { get { return this._nombre; } set { this._nombre = value; } }
+
+        public float Nota { get { return this._nota; } set { this._nota = value; } }
+
+        public Alumno(int legajo, string nombre, string apellido)
+        {
+            this._legajo = legajo;
+            this._nombre = nombre;
+            this._apellido = apellido;
+        }
+
+        private string Mostrar()
+        {
+            return this._legajo.ToString().PadLeft(5,'0') + " - " + this._apellido + ", " + this._nombre + " - Nota: " + this._nota+"\n";
+        }
+
+        public static string Mostrar(Alumno alumno)
+        {
+            return alumno.Mostrar();
+        }
+
+        public static bool operator ==(Alumno a1, Alumno a2)
+        {
+            return Object.Equals(a1.Legajo, a2.Legajo);
+        }
+
+        public static bool operator !=(Alumno a1, Alumno a2)
+        {
+            return !(a1 == a2);
+        }
+
+    }
+}
---
+++ Ahumada.Kevin.2A/Alumno/Materia.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Materia
+    {
+        List<Alumno> _alumnos;
+        EMateria _nombre;
+        static Random _notaParaUnAlumno;
+
+        public List<Alumno> Alumnos { get { return this._alumnos; } set { this._alumnos = value; } }
+
+        public EMateria Nombre { get { return this._nombre; } set { this._nombre = value; } }
+
+        private Materia()
+        {
+            this._alumnos = new List<Alumno>();
+        }
+
+        private Materia(EMateria nombre):this()
+        {
+            this._nombre = nombre;
+        }
+
+        static Materia()
+        {
+            Materia._notaParaUnAlumno = new Random();
+        }
+
+        private string Mostrar()
+        {
+            string retorno = "Materia: " + this._nombre + "\n\n";
+
+            foreach(Alumno a in this._alumnos)
+            {
+                retorno += Alumno.Mostrar(a);
+            }
+
+            return retorno;
+        }
+
+        public void CalificarAlumnos()
+        {
+            foreach(Alumno a in this._alumnos)
+            {
+                a.Nota = Materia._notaParaUnAlumno.Next(0, 10);
+            }
+        }
+
+        public static implicit operator Materia(EMateria nombre)
+        {
+            Materia materia = new Materia(nombre);
+            return materia;
+        }
+
+        public static implicit operator float(Materia m)
+        {
+            float acumulador = 0;
+
+            foreach(Alumno a in m._alumnos)
+            {
+                acumulador += a.Nota;
+            }
+
+            return acumulador / m._alumnos.Count;
+        }
+
+        public static explicit operator string(Materia materia)
+        {
+            return materia.Mostrar();
+        }
+
+        public static bool operator ==(Materia m, Alumno a)
+        {
+            foreach(Alumno alum in m._alumnos)
+            {
+                if(alum == a)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static bool operator !=(Materia m, Alumno a)
+        {
+            return !(m == a);
+        }
+
+        public static Materia operator +(Materia m, Alumno a)
+        {
+            if (m != a)
+            {
+                m._alumnos.Add(a);
+            }
+
+            return m;
+        }
+
+        public static Materia operator -(Materia m, Alumno a)
+        {
+            if (m == a)
+            {
+                m._alumnos.Remove(a);
+            }
+
+            return m;
+        }
+    }
+}
